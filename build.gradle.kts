8a7a9da38f39a8c4a06bd9cbba5c566655b7a9d3
Saneet Bhella <saneet.bhella@zopa.com>
2023-04-17 11:34:42 +0100

    Initial schema registry work

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     kotlin("jvm") version "1.8.20"
+    id("com.github.davidmc24.gradle.plugin.avro") version "1.7.0"
 }
 
 repositories {
@@ -8,13 +9,25 @@ repositories {
 
 subprojects {
     apply(plugin = "kotlin")
+    apply(plugin = "com.github.davidmc24.gradle.plugin.avro")
 
     repositories {
         mavenCentral()
+        maven { url = uri("https://packages.confluent.io/maven") }
     }
 
     dependencies {
+        // Config
+        implementation("com.typesafe:config:1.4.2")
+
+        // Kafka Client
         implementation("org.apache.kafka:kafka-clients:3.4.0")
+
+        // Kafka Avro
+        implementation("org.apache.avro:avro:1.11.1")
+        implementation("io.confluent:kafka-avro-serializer:5.3.0")
+
+        // Logging
         implementation("ch.qos.logback:logback-classic:1.4.6")
     }
 
--- consumer/src/main/kotlin/com/saneetbhella/Consumer.kt
+++ consumer/src/main/kotlin/com/saneetbhella/Consumer.kt
@@ -1,43 +1,19 @@
 package com.saneetbhella
 
-import org.apache.kafka.clients.CommonClientConfigs
-import org.apache.kafka.clients.consumer.ConsumerConfig
+import com.saneetbhella.schemas.Payment
 import org.apache.kafka.clients.consumer.KafkaConsumer
-import org.apache.kafka.common.config.SaslConfigs
-import org.apache.kafka.common.config.SslConfigs
-import org.apache.kafka.common.serialization.StringDeserializer
 import java.time.Duration
 import java.util.Collections
-import java.util.Properties
 
 fun main() {
-    val props = Properties().apply {
-        put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092")
-        put(ConsumerConfig.GROUP_ID_CONFIG, "Sunny")
-        put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer::class.java.name)
-        put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer::class.java.name)
-        put(CommonClientConfigs.SECURITY_PROTOCOL_CONFIG, "SASL_SSL")
-        put(SaslConfigs.SASL_MECHANISM, "PLAIN")
-        put(SslConfigs.SSL_KEYSTORE_LOCATION_CONFIG, "/Users/SaneetBhella/Projects/kafka-docker/certs/kafka/server.keystore.jks")
-        put(SslConfigs.SSL_KEYSTORE_PASSWORD_CONFIG, "kafkadocker")
-        put(SslConfigs.SSL_KEY_PASSWORD_CONFIG, "kafkadocker")
-        put(SslConfigs.SSL_TRUSTSTORE_LOCATION_CONFIG, "/Users/SaneetBhella/Projects/kafka-docker/certs/kafka/server.truststore.jks")
-        put(SslConfigs.SSL_TRUSTSTORE_PASSWORD_CONFIG, "kafkadocker")
-        put(SslConfigs.SSL_ENDPOINT_IDENTIFICATION_ALGORITHM_CONFIG, "")
-
-        val config = """
-            org.apache.kafka.common.security.plain.PlainLoginModule required username='admin' password='admin-secret';
-        """.trimIndent()
-        put(SaslConfigs.SASL_JAAS_CONFIG, config)
-    }
-
-    val kafkaConsumer = KafkaConsumer<String, String>(props)
+    val kafkaConsumer = KafkaConsumer<String, Payment>(AppConfig.kafkaProperties)
     kafkaConsumer.subscribe(Collections.singletonList("payments"))
 
     while (true) {
         val records = kafkaConsumer.poll(Duration.ofMillis(100))
         records.forEach {
-            println(it.value())
+            println(it.value().id)
+            println(it.value().description)
         }
     }
 }
